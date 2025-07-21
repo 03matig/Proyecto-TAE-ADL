4eb8984c631273b8b38cc59df69b9a86023c5d44
03matig <matiasgarinav03@gmail.com>
2025-07-21 03:31:35 -0400

    feat(cypress-checkProducts): valida inicio de sesión exitoso y acceso a la vista de la página que realiza la consulta de Artículos (productos). Al ingresar, se valida el éxito de la prueba en si la tabla es visible y contiene algún valor (es decir, no está vacío).

---
+++ frontend/cypress/e2e/testsProyectoFinal.cy.js
@@ -0,0 +1,32 @@
+// En primer lugar, procederemos a definir las variables base que estaremos utilizando en nuestro set de pruebas.
+const baseUrl = "https://test-adl.leonardojose.dev/";
+const testingEmail = "testeradl@test.com";
+const testingPassword = "Tester@2025";
+
+describe("Set de pruebas 'End to End' (E2E) para Sistema ERP de SELGOM S.A.", () => {
+    // Lo primero que haremos, será establecer un Hook Iterativo base que realice un patrón antes de cada prueba, en este caso, el inicio de sesión.
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.visit(baseUrl); // Accedemos al sitio
+        cy.get("#root > div > div > div > img").should("be.visible"); // Validamos que haya cargado el DOM verificando que esté visible un elemento del HTML que debiera estar visible como prioridad al cargar la página.
+        cy.get("#email").type(testingEmail); // Obtenemos campo de entrada y escribimos el correo.
+        cy.get("#password").type(testingPassword); // Obtenemos campo de entrada y escribimos la contraseña.
+        cy.get("#root > div > div > form > div:nth-child(4) > button").click(); // Damos click al botón de Ingresar
+        
+        cy.get("#root > div > div > main > div > p")
+            .should("contain", "Bienvenido al sistema ERP."); // Validación de nuestro procedimiento de Inicio de Sesión.
+    });
+
+    it("Inicio de sesión y consulta de productos", () => {
+        cy.get("#root > div > aside > nav > ul > li:nth-child(2) > div > div").should("be.visible").click();
+        cy.get("#root > div > aside > nav > ul > li:nth-child(2) > div > ul > li:nth-child(3) > a").should("be.visible").click();
+        cy.get("#root > div > div > main > div > div.sm\\:flex.sm\\:items-center > div.sm\\:flex-auto > h1").should("contain", "Listado de Artículos"); // Validación intermedia, acceso a la vista de Artículos.
+
+        // Y establecemos las condiciones para la validación final
+        cy.get("#root > div > div > main > div > div.mt-6 > div.hidden.lg\\:block > div > table > tbody > tr:nth-child(1) > td:nth-child(1)")
+            .should("be.visible")
+            .and(($el) => {
+                expect($el.text().trim()).to.not.be.empty
+            });
+    });
+});
